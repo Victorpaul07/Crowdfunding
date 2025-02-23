c84d2a1890e84079640fd9847eec45d3c05ab5e1
Victorpaul07 <125550981+Victorpaul07@users.noreply.github.com>
2025-02-23 19:03:14 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="footer">
+        <p>&copy; {new Date().getFullYear()} Crowdfunding. All Rights Reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+      <header className="text-center p-4 bg-primary text-white">
+        <h1>Crowdfunding</h1>
+        <p>Fund your dreams or support projects</p>
+      </header>
+    );
+  };
+  
+  export default Header;
+  
